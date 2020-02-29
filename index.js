db893735786dd0d894c608dea4f43b38f24ffdd9
Cara Bunnell <carabunnell@gmail.com>
2020-02-29 13:41:34 -0800

    pushing generator

---
+++ index.js
@@ -0,0 +1,100 @@
+const fs = require("fs");
+const util = require("util");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+// inquirer
+// .prompt({
+//     message: "Enter your GitHub username:",
+//     name: "username"
+// })
+// .then(function({ username }) {
+//     const queryUrl = `https://api.github.com/users/${username}`;
+//     console.log("api queired");
+
+// axios.get(queryUrl).then(function(res) {
+//     console.log(res.data.email); //attempting to gather git email
+//     console.log(res.data.image); // attempting to gather image info
+// })
+// });
+
+const writeFileAsynch = util.promisify(fs.writeFile);
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "username",
+            message: "Enter your GitHub username:",
+        },
+        // {
+        //     type: "input",
+        //     name: "badge",
+        //     message: "At least one badge."
+        // },
+        {
+            type: "input",
+            name: "title",
+            message: "what is your project title?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "what is your project description?"
+        },
+        {
+            type: "input",
+            name: "tableof",
+            message: "what is your table of contents?"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "what is your installation?"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "what is your usage?"
+        },
+        {
+            type: "input",
+            name: "license",
+            message: "what is your license?"
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "who is contributing?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "what are your tests?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "any questions?"
+        },
+    ]);
+}
+
+
+
+function createReadme(answers) {
+    return `# readme_generator\nBadge: $___badge here__$\nProject Title: ${answers.title}\nDescription: ${answers.description}\nTable of Contents: ${answers.tableof}\nInstallation: ${answers.installation}\nUsage: ${answers.usage}\nLicense: ${answers.license}\nContributors: ${answers.contributing}\nTests: ${answers.tests}\nQuestions: ${answers.questions}`;
+}
+
+promptUser()
+    .then(function(answers) {
+        const md = createReadme(answers);
+
+        return writeFileAsynch("readme/readme.md", md);
+    })
+    .then(function() {
+        console.log("Successfully wrote to readme");
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
---
+++ readme/readme.md
@@ -0,0 +1,11 @@
+# readme_generator
+Badge: $___badge here__$
+Project Title: asdf
+Description: asdf
+Table of Contents: asdf
+Installation: asdf
+Usage: asdf
+License: asdf
+Contributors: sadf
+Tests: asdf
+Questions: asdf
